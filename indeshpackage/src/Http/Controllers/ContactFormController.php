c93b21a39e97eee0cdc5a24d20a7c3302f9a9bad
Indesh Prinja <indeshducktale@gmail.com>
2019-02-07 17:38:19 +0530

    setup

---
+++ indeshpackage/src/Http/Controllers/ContactFormController.php
@@ -0,0 +1,21 @@
+<?php
+namespace indeshvendor\indeshpackage\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use indeshvendor\indeshpackage\Models\ContactForm;
+
+class ContactFormController extends Controller {
+
+    public function index()
+    {
+       return view('contactform::contact');
+    }
+
+    public function sendMail(Request $request)
+    {
+        ContactForm::create($request->all());
+
+    	return redirect(route('contact'))->with(['message' => 'Thank you, your mail has been sent successfully.']);
+    }
+}
---
+++ indeshpackage/src/Models/ContactForm.php
@@ -0,0 +1,10 @@
+<?php
+namespace indeshvendor\indeshpackage\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ContactForm extends Model
+{
+    protected $guarded = [];
+    protected $table = 'contact';
+}
